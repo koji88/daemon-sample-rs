6227f1f8df37c1f6adb320f59426217d0ec36b20
Koji Hachiya <koji3.hachiya@toshiba.co.jp>
2016-10-17 11:20:28 +0000

    add env_logger

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,42 @@
 extern crate nix;
 #[macro_use] extern crate chan;
 extern crate chan_signal;
+#[macro_use] extern crate log;
+extern crate env_logger;
 
 use nix::unistd::*;
 use chan_signal::{Signal,notify};
 
 fn main() {
-    match fork().expect("fork faild") {
-        ForkResult::Parent{child} => {
-            println!("Start Daemon Process pid={}",child);
-        }
-        ForkResult::Child =>{
-            let signal = notify(&[Signal::INT, Signal::TERM]);
-            run(signal);
+    env_logger::init().unwrap();
+    
+    match fork() {
+        Ok(result) => match result {
+            ForkResult::Parent{child} => {
+                info!("Start Daemon Process pid={}",child);
+            }
+            ForkResult::Child =>{
+                let signal = notify(&[Signal::INT, Signal::TERM]);
+                run(signal);
+            }
+        },
+        Err(_) => {
+            error!("Cannot fork child.")
         }
     }
 }
 
 fn run(signal: chan::Receiver<Signal>) {
-    println!("Child process is started.");
+    info!("Child process is started.");
     loop {
         chan_select! {
             default => {
-                println!("child: sleep 3sec...");
+                info!("child: sleep 3sec...");
                 sleep(3);
             },
             signal.recv() => break,
         }
     }
-    println!("Child process is terminated.");
+    info!("Child process is terminated.");
 }
 
